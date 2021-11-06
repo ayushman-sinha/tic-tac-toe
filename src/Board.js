9ed4b95420d1603fc0dcf6c7d0e260fccac12ea7
Ayushman Sinha <ayushman.sinhaa@gmail.com>
2021-11-07 01:47:31 +0530

    RedirectFIx

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,7 @@
 import Square from './Square';
 import React from 'react';
 import './board.css';
-
+import './buttonC.css';
 const Board = ({ squares, onClick }) => (
     <div className="mainContainer">
 	<div className="container">
--- src/HomeButtons.js
+++ src/HomeButtons.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import  './board.css';
 import './buttonC.css';
 import Particles from "react-tsparticles";
 import particleConfig from './particleConfig';
@@ -7,16 +7,29 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Game from './Game';
 const HomeButtons = () => {
     const [isStart, setIsStart] = useState(false);
-     
+    
+     if(!isStart)
+     {
     return (
         
         <div className="ButtonContainer">
              <div className="box" onClick={() => setIsStart(true)}><h1>Start</h1></div>
             <div className="box"  ><h1>Rules</h1></div>
             <div className="box" onClick={() => setIsStart(false)}><h1>Exit</h1></div>
-            {isStart ? <Game /> : null }
+            {isStart ?  <div className="GameContainer"><Game ></Game></div>: null}
+           
         </div>
+        
     )
+     }
+     else
+     {
+         return(
+        <div >
+   <Game ></Game>  
+            </div>)
+     }
+
 }
 
 export default HomeButtons
