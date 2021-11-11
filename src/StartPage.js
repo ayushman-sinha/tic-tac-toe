515bba3a5ba9e271566b8a64fea31d06eccbeb54
Ayushman Sinha <ayushman.sinhaa@gmail.com>
2021-11-12 00:49:18 +0530

    Deploy1

--- src/StartPage.js
+++ src/StartPage.js
@@ -2,8 +2,6 @@ import { useState } from "react";
 import './buttonC.css';
 import Particles from "react-tsparticles";
 import particleConfig from './particleConfig';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Game from './Game';
 import HomeButtons from './HomeButtons';
 
 const StartPage = () => {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 import App from './App';
 
 
 ReactDOM.render(
+ 
   <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>,
+    
+   
+  </React.StrictMode>
+  ,
   document.getElementById('root')
 );
